8ee1db19aaa3f571a92add72324b05dd867c4fb1
ripvanchuckle <71681035+ripvanchuckle@users.noreply.github.com>
2020-09-28 21:27:24 -0500

    Add files via upload

---
+++ file_creation.py
@@ -0,0 +1,26 @@
+import os.path
+
+path = str(input("Please provide a directory to save your file to: "))
+
+isdir = str(os.path.isdir(path))
+
+if isdir == 'True':
+	print("Directory exists!")
+	print()
+	file = input("Please provide a file name: ")
+	name = input("Please tell me your name: ")
+	address = input("Please tell me your address: ")
+	phone = input("Please tell me your phone number: ")
+	os.chdir(path)
+	with open(file, 'w') as file_object:
+		file_object.write((name + ","))
+		file_object.write((address + ","))
+		file_object.write((phone))
+	print()
+	print(f' File contents of {path}\\{file}', "are" )
+	print()
+	f = open((file), "r")
+	print(f.read())
+	f.close()
+elif isdir == 'False':
+	print("Directory does not exist.")
