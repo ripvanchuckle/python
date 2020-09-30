ab80989a5971f068ffb8611c742ba30e1f2898f1
ripvanchuckle <71681035+ripvanchuckle@users.noreply.github.com>
2020-09-30 18:33:01 -0500

    Add files via upload

--- file_creation.py
+++ file_creation.py
@@ -23,4 +23,23 @@ if isdir == 'True':
 	print(f.read())
 	f.close()
 elif isdir == 'False':
-	print("Directory does not exist.")
+	dirct = input("Directory does not exist, do you wish to create it? ")
+	if dirct == 'Y':
+		os.mkdir(path)
+		file = input("Please provide a file name: ")
+		name = input("Please tell me your name: ")
+		address = input("Please tell me your address: ")
+		phone = input("Please tell me your phone number: ")
+		os.chdir(path)
+		with open(file, 'w') as file_object:
+			file_object.write((name + ","))
+			file_object.write((address + ","))
+			file_object.write((phone))
+		print()
+		print(f' File contents of {path}\\{file}', "are" )
+		print()
+		f = open((file), "r")
+		print(f.read())
+		f.close()
+	else:
+		print("Program cannot continue. Exiting.")
